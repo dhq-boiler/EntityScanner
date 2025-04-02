f9b31d0e6a6d72910802eb3fa511a3e24da742d0
dhq_boiler <dhq_boiler@live.jp>
2025-04-02 17:24:27 +0900

    bugfix

---
+++ EntityScanner/IEntitySerializable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityScanner
+{
+    /// <summary>
+    /// カスタム型をEntityScannerで処理するためのインターフェース
+    /// </summary>
+    public interface IEntitySerializable
+    {
+        /// <summary>
+        /// シードデータとして使用可能な文字列表現を返す
+        /// </summary>
+        string ToEntityString();
+
+        /// <summary>
+        /// 文字列表現からインスタンスを生成する
+        /// </summary>
+        static abstract IEntitySerializable FromEntityString(string value);
+    }
+}
